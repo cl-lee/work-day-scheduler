efc480d171c1968379110b4d09324f8939dab1a5
cl.lee <cl.lee.ofz@gmail.com>
2023-01-22 15:50:23 +0000

    updated hours array and associated codes

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-// --- array for setting and storing the hours on display in the calendar ---
-// sets the starting hour, and the number of timeblocks in the calendar
-let startingHour = 9;
-let timeblocksQuantity = 9;
-
-// hours arrays for displaying the hours in timeblocks
-let hours = [];
-for (let i = 0; i < timeblocksQuantity; i++) {
-    const hour = startingHour + i;
-    hourInMoment = moment().set({"h": hour, "m": 0, "s": 0}).format("hA");
-    hours.push(hourInMoment);
-}
-
-// Display the current day at the top of the calender
+// --- Display the current day at the top of the calender ---
 let today = moment();
 $("#currentDay").text(today.format("dddd, Do MMMM YYYY"));
 
-// Present timeblocks for standard business hours when the user scrolls down.
+// --- array for setting and storing the hours on display in the calendar ---
+// sets the starting hour, and the number of timeblocks in the calendar
+let startingHour = 9;
+let timeblocksAmount = 9;
 
+// hours arrays for displaying the hours in timeblocks
+let hours = [];
+for (let hour = startingHour; hour < startingHour + timeblocksAmount; hour++) {
+    hours.push(hour);
+}
+
+// --- Present timeblocks for standard business hours when the user scrolls down. ---
 // codes for a timeblock
-for (let j = 0; j < timeblocksQuantity; j++) {
+for (let i = 0; i < timeblocksAmount; i++) {
+    
+    let hourInMoment = moment().set({"h": hours[i], "s": 0, "ms": 0}).format("hA");
 
     let $block = $("<div>");
     $block.attr("class", "row");
     $(".container").append($block);
 
     let $hour = $("<time>");
-    $hour.text(hours[j]);
+    $hour.text(hourInMoment);
     $hour.attr("class", "hour col-1");
-    $hour.attr("data-time", hours[j])
+    $hour.attr("data-time", hourInMoment);
     $block.append($hour);
 
-    let $eventDescriptionEL = $("<textarea>");
+    let $eventDescriptionEL = $("<textarea>")
     $eventDescriptionEL.attr("class", "description col-10");
     $eventDescriptionEL.text(localStorage.getItem($hour.attr("data-time")));
     $block.append($eventDescriptionEL);
@@ -41,6 +40,15 @@ for (let j = 0; j < timeblocksQuantity; j++) {
     $saveButton.append($buttonIcon);
     $saveButton.attr("class", "saveBtn col-1 saveAction");
     $block.append($saveButton);
+
+    // Color-code each timeblock based on past, present, and future when the timeblock is viewed.
+    if (today.isAfter(hourInMoment, "hour")) {
+        $eventDescriptionEL.addClass("past");
+    } else if (today.isSame(hourInMoment, "hour")) {
+        $eventDescriptionEL.addClass("present");
+    } else {
+        $eventDescriptionEL.addClass("future");
+    }
 }
 
 // stores event and time when clicking on the "save" button
@@ -50,22 +58,10 @@ $("button").on("click", function (event) {
     let $eventTime = $(event.target).parentsUntil(".container").children(".hour").attr("data-time");
 
     localStorage.setItem($eventTime, $eventDescription);
+    console.log(localStorage.getItem($eventTime));
 })
 
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed.
-// if (today.isSame(hours[0], "hour")) {
-//     console.log("true");
-// } else {
-//     console.log("false");
-// }
 
-// if (today.isAfter(hours[0], "hour")) {
-//     $eventDescriptionEL.addClass("past");
-// } else if (today.isSame(hours[0], "hour")) {
-//     $eventDescriptionEL.addClass("present");
-// } else {
-//     $eventDescriptionEL.addClass("future");
-// }
 
 
 
